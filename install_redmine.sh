5e7a03a2ea03cf1c11d84eb264635e707501c83e
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2018-04-06 15:57:47 -0300

    testa se ruby esta instalado

--- install_redmine.sh
+++ install_redmine.sh
@@ -101,16 +101,18 @@ mariadb-server
 
 ## RUBY
 
-yum -y install gcc 
-cd /usr/local/src
-wget https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.0.tar.gz
-tar xvfz ruby-2.5.0.tar.gz
-cd ruby-2.5.0/
-./configure && make && make install
+RUBY=`ruby -v`
+if [ ! $? -eq ]; then 
+  yum -y install gcc 
+  cd /usr/local/src
+  wget https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.0.tar.gz
+  tar xvfz ruby-2.5.0.tar.gz
+  cd ruby-2.5.0/
+  ./configure && make && make install
 
 export PATH=/usr/local/bin:$PATH
-ruby -v
-#ruby 2.5.0p0 (2017-12-25 revision 61468) [x86_64-linux]
+# ruby -v
+# ruby 2.5.0p0 (2017-12-25 revision 61468) [x86_64-linux]
 
 
 
